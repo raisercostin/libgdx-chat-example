8ffc5bc6dea88c3648f604fa3309a0046dbd881a
raisercostin <raisercostin@gmail.com>
2016-11-13 16:36:10 +0200

    Upgrade gwt.

--- chat/html/src/main/java/org/stofkat/chat/html/ChatHtml.java
+++ chat/html/src/main/java/org/stofkat/chat/html/ChatHtml.java
@@ -6,14 +6,14 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 
 public class ChatHtml extends GwtApplication {
-	
-	@Override
-	public ApplicationListener getApplicationListener () {
-		return new HtmlChat();
-	}
-	
+
 	@Override
 	public GwtApplicationConfiguration getConfig () {
 		return new GwtApplicationConfiguration(480, 320);
+	}
+
+	@Override
+	public ApplicationListener createApplicationListener() {
+		return new HtmlChat();
 	}
 }
